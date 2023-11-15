33f1c25e94e9136220e83babc30b9f2dd05a1660
fcortes10 <fcortes@pucp.edu.pe>
2023-11-14 19:09:36 -0500

    agregar use efect en property

--- src/componentes/Pages/Property.jsx
+++ src/componentes/Pages/Property.jsx
@@ -22,6 +22,17 @@ const Property = () => {
   };
 
 
+  //Función para el embed Property
+  useEffect(() => {
+    var divElement = document.getElementById('viz1682537459047');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '1200px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '710px'; vizElement.style.maxHeight = (divElement.offsetWidth * 0.75) + 'px'; }
+    else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '1200px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '710px'; vizElement.style.maxHeight = (divElement.offsetWidth * 0.75) + 'px'; }
+    else { vizElement.style.width = '100%'; vizElement.style.minHeight = '1450px'; vizElement.style.maxHeight = (divElement.offsetWidth * 1.77) + 'px'; }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);
+  }, []);
+
 
   return (
     <div className='home-inver'>
