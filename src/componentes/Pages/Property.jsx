24f8bb0558f6b1586c9eb5105bca4d29e311edb9
fcortes10 <fcortes@pucp.edu.pe>
2023-11-14 19:04:55 -0500

    cambio del dashboard de property

--- src/componentes/Pages/Property.jsx
+++ src/componentes/Pages/Property.jsx
@@ -88,12 +88,12 @@ const Property = () => {
                 <li className="nav-item">
 
                   <div className=" container-sm ">
-                      <div className="card-docs-m  " onClick={handleLogout}>                  
-                        <div class="card-body-docs col-2 "  >
-                          <img src={Ilogout} className="" alt="" width="24px" height="24px" />
-                        </div>
-                        <br />                    
+                    <div className="card-docs-m  " onClick={handleLogout}>
+                      <div class="card-body-docs col-2 "  >
+                        <img src={Ilogout} className="" alt="" width="24px" height="24px" />
                       </div>
+                      <br />
+                    </div>
                   </div>
                 </li>
               </ul>
@@ -108,14 +108,22 @@ const Property = () => {
       <div className="centrado">
         {/*Embed tableau*/}
         <div className=" boards">
-          <tableau-viz id='tableau-viz' src='https://prod-useast-a.online.tableau.com/t/nuestrocasa/views/assets_profile/Assetsprofile'
-            width='1250' height='730' hide-tabs toolbar='bottom' >
-
-          </tableau-viz>
-          <br />
-          <tableau-viz id='tableau-viz' src='https://prod-useast-a.online.tableau.com/t/nuestrocasa/views/portfolio_v1/Property'
-            width='1250' height='723' toolbar='bottom' >
-          </tableau-viz>
+          <div class='tableauPlaceholder' id='viz1682537459047' >
+            <noscript>
+              <a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;po&#47;portfolio_pub&#47;Property&#47;1_rss.png' style='border: none' /></a></noscript>
+            <object class='tableauViz'  >
+              <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+              <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='portfolio_pub&#47;Property' />
+              <param name='tabs' value='yes' />
+              <param name='toolbar' value='yes' />
+              <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;po&#47;portfolio_pub&#47;Property&#47;1.png' />
+              <param name='animate_transition' value='yes' />
+              <param name='display_static_image' value='yes' />
+              <param name='display_spinner' value='yes' />
+              <param name='display_overlay' value='yes' /><param name='display_count' value='yes' />
+              <param name='language' value='en-US' />
+            </object>
+          </div>
         </div>
       </div>
       <br />
