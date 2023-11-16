455c562d207d7671c5f2b0c1bdbcafd0d34ffcd6
fcortes10 <fcortes@pucp.edu.pe>
2023-11-15 19:31:28 -0500

    cambios en detalle para que muestre otra página de tableau

--- src/componentes/Pages/Detalle.jsx
+++ src/componentes/Pages/Detalle.jsx
@@ -20,13 +20,14 @@ const Customer = () => {
 
   //Función para el embed Buyer
   useEffect(() => {
-    var divElement = document.getElementById('viz1682537459047');
+    var divElement = document.getElementById('viz1700094002296');
     var vizElement = divElement.getElementsByTagName('object')[0];
     if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '1200px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '710px'; vizElement.style.maxHeight = (divElement.offsetWidth * 0.75) + 'px'; }
     else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '1200px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '710px'; vizElement.style.maxHeight = (divElement.offsetWidth * 0.75) + 'px'; }
-    else { vizElement.style.width = '100%'; vizElement.style.minHeight = '1450px'; vizElement.style.maxHeight = (divElement.offsetWidth * 1.77) + 'px'; }
+    else { vizElement.style.minWidth = '1200px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '710px'; vizElement.style.maxHeight = (divElement.offsetWidth * 1.77) + 'px'; }
     var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
   }, []);
 
 
@@ -105,20 +106,26 @@ const Customer = () => {
         <br />
         <div>
           {/*Embed buyer*/}
-          <div class='tableauPlaceholder' id='viz1682537459047' >
+          <div class='tableauPlaceholder' id='viz1700094002296' style='position: relative'>
             <noscript>
-              <a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;po&#47;portfolio_pub&#47;Buyer&#47;1_rss.png' style='border: none' /></a></noscript>
-            <object class='tableauViz'  >
+              <a href='#'>
+                <img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;po&#47;portfolio_pub&#47;Selection&#47;1_rss.png' style='border: none' />
+              </a>
+            </noscript>
+            <object class='tableauViz' style='display:none;'>
               <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-              <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='portfolio_pub&#47;Buyer' />
+              <param name='embed_code_version' value='3' />
+              <param name='site_root' value='' />
+              <param name='name' value='portfolio_pub&#47;Selection' />
               <param name='tabs' value='yes' />
               <param name='toolbar' value='yes' />
-              <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;po&#47;portfolio_pub&#47;Buyer&#47;1.png' />
+              <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;po&#47;portfolio_pub&#47;Selection&#47;1.png' />
               <param name='animate_transition' value='yes' />
               <param name='display_static_image' value='yes' />
               <param name='display_spinner' value='yes' />
-              <param name='display_overlay' value='yes' /><param name='display_count' value='yes' />
-              <param name='language' value='en-US' />
+              <param name='display_overlay' value='yes' />
+              <param name='display_count' value='yes' />
+              <param name='language' value='es-ES' />
             </object>
           </div>
 
@@ -136,5 +143,9 @@ const Customer = () => {
 
   )
 }
+
+
+
+
 
 export default Customer
