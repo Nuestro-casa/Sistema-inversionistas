9be4d26ff17b082f663cf4c5eae12929d45e27cf
aarevaloe <121042571+aarevaloe@users.noreply.github.com>
2024-02-14 11:20:41 -0500

    Commit-prueba

--- src/componentes/Pages/Login.jsx
+++ src/componentes/Pages/Login.jsx
@@ -38,7 +38,7 @@ function Login() {
             const options = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: '{"email":"'+datos.email+'","password":"'+datos.password+'"}'
+                body: '{"email":"' + datos.email + '","password":"' + datos.password + '"}'
             };
 
             fetch('https://sistema-duppla-backend.herokuapp.com/inv/login', options).then(response => response.json())
@@ -117,79 +117,14 @@ function Login() {
                                         </div>
                                         <button type="submit"
                                             className="btn btn-prueba-ingreso text-center links text-white btn-mover ">Siguiente</button>
-
-
-
                                     </form>
-
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
-
-
             </div>
-
-
-
-
-
-            {/* <div className="arrow-return">
-                <Link to='/singin'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="64px" height="64px" fill="currentColor" className=" arrow-return bi bi-arrow-left-short" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
-                    </svg>
-                </Link>
-            </div>
-            <p>{String(authenticated)}</p>
-            <div className="title-register">
-                <h2> <b>Iniciar Sesión</b>
-                </h2>
-            </div>*/}
-            {/*formulario registro 
-            <div className="form-register  centrado container-sm">
-                <form onSubmit={handleSubmit} >
-                    <div className="mb-3">
-                        <input type="email"
-                            name='email'
-
-                            onChange={handleInputChance}
-                            value={datos.email}
-
-                            className="form-control input-register-inver"
-                            id="exampleInputEmail1"
-                            placeholder="Correo electrónico"
-                            aria-describedby="emailHelp"
-                            required />
-
-                    </div>
-                    <div className="mb-3">
-
-                        <input type="password"
-                            name='password'
-                            onChange={handleInputChance}
-                            value={datos.password}
-                            className="form-control input-register-inver"
-                            placeholder="Contraseña"
-                            id="exampleInputPassword1" />
-
-                    </div>
-                    <button type="submit"
-                        className="btn btn-prueba-ingreso text-center links text-white btn-mover ">Siguiente</button>
-
-
-
-                </form>
-
-            </div>*/}
-
         </div>
-
-
-
-
     )
 }
 
